fb9cab69dc2c619930bbe92f1d809f35ab813930
Zahar Vaschenko <jnjhnjmj@outlook.com>
2024-06-30 22:29:18 +0200

    first version

---
+++ PhotoGallery/PhotoGallery.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PhotoGallery.aspx.cs" Inherits="PhotoGallery.PhotoGallery" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Photo Gallery</title>
+    <style>
+        .gallery-container {
+            text-align: center;
+            margin: 20px;
+        }
+        .gallery-image {
+            max-width: 80%;
+            max-height: 500px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="gallery-container">
+            <asp:Image ID="imgPhoto" runat="server" CssClass="gallery-image" />
+            <br />
+            <asp:Label ID="lblFileName" runat="server"></asp:Label>
+            <br /><br />
+            <asp:Button ID="btnFirst" runat="server" Text="First" OnClick="btnFirst_Click" />
+            <asp:Button ID="btnPrevious" runat="server" Text="Previous" OnClick="btnPrevious_Click" />
+            <asp:Button ID="btnNext" runat="server" Text="Next" OnClick="btnNext_Click" />
+            <asp:Button ID="btnLast" runat="server" Text="Last" OnClick="btnLast_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ PhotoGallery/PhotoGallery.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PhotoGallery
+{
+    public partial class PhotoGallery : System.Web.UI.Page
+    {
+        private string[] _images;
+        private int _currentIndex;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadImages();
+                DisplayImage(0);
+            }
+            else
+            {
+                _images = (string[])ViewState["Images"];
+                _currentIndex = (int)ViewState["CurrentIndex"];
+            }
+        }
+
+        private void LoadImages()
+        {
+            string imagesFolder = Server.MapPath("~/Images");
+            _images = Directory.GetFiles(imagesFolder);
+            ViewState["Images"] = _images;
+            ViewState["CurrentIndex"] = 0;
+        }
+
+        private void DisplayImage(int index)
+        {
+            if (_images != null && _images.Length > 0)
+            {
+                _currentIndex = index;
+                string relativePath = $"~/Images/{Path.GetFileName(_images[index])}";
+                imgPhoto.ImageUrl = relativePath;
+                lblFileName.Text = Path.GetFileName(_images[index]);
+                ViewState["CurrentIndex"] = _currentIndex;
+            }
+        }
+
+        protected void btnFirst_Click(object sender, EventArgs e)
+        {
+            DisplayImage(0);
+        }
+
+        protected void btnPrevious_Click(object sender, EventArgs e)
+        {
+            _currentIndex = (int)ViewState["CurrentIndex"];
+            if (_currentIndex > 0)
+            {
+                DisplayImage(_currentIndex - 1);
+            }
+        }
+
+        protected void btnNext_Click(object sender, EventArgs e)
+        {
+            _currentIndex = (int)ViewState["CurrentIndex"];
+            if (_currentIndex < _images.Length - 1)
+            {
+                DisplayImage(_currentIndex + 1);
+            }
+        }
+
+        protected void btnLast_Click(object sender, EventArgs e)
+        {
+            DisplayImage(_images.Length - 1);
+        }
+    }
+}
